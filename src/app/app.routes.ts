2f49157eaf39bc0e73e3988c1031bba7875d4d82
JuanmaH198 <juanmah1998@gmail.com>
2025-06-30 21:41:18 +0200

    re estructuracion para angular 18

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { LandingComponent } from './components/routes/landing/landing.component';
+import { LandingComponent } from './routes/landing/landing.component';
 
 export const routes: Routes = [
      { path: '', component: LandingComponent }, 
