b6cc23e57c4c84690b3376cae84478a944c45f4b
JuanmaH198 <juanmah1998@gmail.com>
2025-06-30 22:10:23 +0200

    update finalizado a angular 19

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-signup',
-  standalone: true,
-  imports: [],
-  templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+    selector: 'app-signup',
+    imports: [],
+    templateUrl: './signup.component.html',
+    styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
 
--- src/app/routes/landing/landing.component.ts
+++ src/app/routes/landing/landing.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-landing',
-  standalone: true,
-  imports: [],
-  templateUrl: './landing.component.html',
-  styleUrl: './landing.component.scss'
+    selector: 'app-landing',
+    templateUrl: './landing.component.html',
+    styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
 
